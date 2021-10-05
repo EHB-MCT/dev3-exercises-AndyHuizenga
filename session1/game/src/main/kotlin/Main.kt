51797ee06c3c349964fabd09e375fa2d7086754e
AndyHuizenga <andy.huizenga@student.ehb.be>
2021-10-05 14:32:31 +0200

    session1

---
+++ session1/game/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+import kotlin.random.Random
+
+var options = arrayOf("rock","scissors","paper")
+var pcWeapon = options.random()
+var userChoice = getUserPick(options)
+
+print
+
+
+
+fun main() {
+
+}
+
+
+
+
--- session1/game/src/main/kotlin/gameS.kt
+++
@@ -1,9 +0,0 @@
-import kotlin.random.Random
-
-val chosenWeapon = arrayOf("rock","scissors","paper")
-
-val pcWeapon = chosenWeapon [(Random.nextInt(0,3))]
-
-fun show(){
-    println(pcWeapon)
-}
